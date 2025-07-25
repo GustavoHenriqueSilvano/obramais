a6dc60c7f1028973849a618126405258eff97e8b
Gustavo Henrique <gustavo.silvano@outlook.com.br>
2025-07-24 21:20:24 -0300

    Criada classe para cores padrões do app

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CoresApp {
+  // definição de cores para estilo default / claro
+
+  static const Color backgroundClaro = Color(0xFFD4EDEC); // Light Cyan
+  static const Color corFonteClaro = Color(0xFF070922); // Oxford Blue
+  static const Color corAuxiliar = Color(0xFF163C18); // Pakistan Green
+  static const Color corAuxiliar2 = Color(0xFF418945); // Sea Green
+}
